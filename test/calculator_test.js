72faf13553aed8a003d6e058171daaf8930a9f0f
echooocc <echo2go@gmail.com>
2016-11-14 16:03:06 -0500

    add missing case for electronics

--- test/calculator_test.js
+++ test/calculator_test.js
@@ -10,6 +10,7 @@ describe('Calculator', function() {
             expect(ca.calculate(1299.99, 3, 'food')).to.equal(1591.58);
             expect(ca.calculate(5432.00, 1, 'drugs')).to.equal(6199.81);
             expect(ca.calculate(12456.95, 4, 'books')).to.equal(13707.63);
+            expect(ca.calculate(499.99, 1, 'electronics')).to.equal(541.79);
         });
 
         it('should treat default labor markup count for 1 people if the people input option is missing', function() {
