76d458083a18e247daacc8f75e150b230eba1020
echooocc <echo2go@gmail.com>
2016-11-14 09:44:29 -0500

    rename test file

--- test/flatmarkup_test.js
+++ test/flatmarkup_test.js
@@ -5,9 +5,15 @@ var FlatMarkup =  require('../calculator/flatmarkup.js');
 
 describe('FlatMarkup', function() {
     var fm = new FlatMarkup();
-    describe('#add()', function () {
+    describe('#calculate()', function () {
         it('should return flat markup without exception', function() {
             expect(fm.calculate(100)).to.equal(5);
+        });
+    });
+
+    describe('#addson()', function () {
+        it('should return the price with base and flat markup', function() {
+            expect(fm.addson(100)).to.equal(105);
         });
     });
 });
