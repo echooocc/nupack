bc53284edf28eed585829dfca9effbef9a9be889
echooocc <echo2go@gmail.com>
2016-11-14 08:20:17 -0500

    add flatmarkup calculator test

---
+++ calculator/flatmarkup.js
@@ -0,0 +1,7 @@
+var FlatMarkup = function() {};
+
+FlatMarkup.prototype.calculate = function(base) {
+    return base*0.05;
+}
+
+module.exports = FlatMarkup;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,13 @@
 var chai = require('chai');
 var expect = chai.expect;
-var should = chai.should();
+
+var FlatMarkup =  require('../calculator/flatmarkup.js');
+
+describe('FlatMarkup', function() {
+    var fm = new FlatMarkup();
+    describe('#add()', function () {
+        it('should return flat markup without exception', function() {
+            expect(fm.calculate(100)).to.equal(5);
+        });
+    });
+});
