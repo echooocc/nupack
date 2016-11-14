0537f1047e506f84a8ac80e97424033e7b219f8e
echooocc <echo2go@gmail.com>
2016-11-14 09:46:44 -0500

    addson for calculate flat markup and base price

--- calculator/flatmarkup.js
+++ calculator/flatmarkup.js
@@ -4,4 +4,8 @@ FlatMarkup.prototype.calculate = function(base) {
     return base*0.05;
 }
 
+FlatMarkup.prototype.addson = function(base) {
+    return this.calculate(base)+base;
+}
+
 module.exports = FlatMarkup;
