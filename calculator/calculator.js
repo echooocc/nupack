f3bf232bd2fd4308b27a004b5a5e7c8d0c6316f2
echooocc <echo2go@gmail.com>
2016-11-22 23:25:18 -0500

    move check categories function to helper.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,50 +2,20 @@
 
 var MarkupCalculator = require('./markupcalculator.js');
 var MarkupConfig = require('./markup_config.js');
+var Helper = require('./helper.js');
 
 var mc = new MarkupCalculator();
+var helper = new Helper();
 
 var markups = MarkupConfig.MarkupConfig;
 var categories = MarkupConfig.CategoriesConfig;
 
 var Calculator = function() {
 
-  //round output to 2 decimal place
-  function numberFormat(number) {
-    return parseFloat(number.toFixed(2));
-  }
-
-  function checkPartialParameter(args) {
-    let parameters = {};
-    if (typeof args[0] !== "number" || args[0] <= 0) {
-      throw new Error("Invalid base price input");
-    }
-    parameters.base = args[0];
-    if (args.length < 3) {
-      if (typeof args[1] === "string") {
-        parameters.category = args[1].toLowerCase();
-      }
-      parameters.ppl = 1;
-    } else {
-      parameters.ppl = args[1];
-      parameters.category = args[2].toLowerCase();
-    }
-    return parameters;
-  }
-
-  function checkCategories(category) {
-    for (let i in categories) {
-      if (categories[i].includes(category)) {
-        return i;
-      }
-    }
-    return undefined;
-  }
-
   return {
     calculate: function(base, ppl, category) {
       //format the arguments incase of partial parameters
-      let args = checkPartialParameter(arguments);
+      let args = helper.checkPartialParameter(arguments);
 
       let flatbase = mc.getFlatBase(args.base, markups.FLAT_MARKUP_RATE);
       let total = 0;
@@ -53,11 +23,11 @@ var Calculator = function() {
       total = flatbase + mc.getLaborMarkup(flatbase, markups.LABOR_MARKUP_RATE, args.ppl);
 
       //match category with alternative names
-      let matchCategory = checkCategories(args.category);
+      let matchCategory = helper.checkCategories(args.category, categories);
       if (matchCategory !== undefined) {
         total += mc.getOtherMarkup(flatbase, markups[matchCategory]);
       }
-      return numberFormat(total);
+      return helper.numberFormat(total);
     }
   };
 };
---
+++ calculator/helper.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var Helper = function() {
+
+  return {
+    //round output to 2 decimal place
+    numberFormat: function(number) {
+      return parseFloat(number.toFixed(2));
+    },
+    /*check partial input
+     * 1,throws error when input is not number or empty
+     * 2,accpet input format 5432.00, 1, 'drug'; 5432.00, 1; 5432.00, 'drugs'; 5432.00
+     */
+    checkPartialParameter: function(args) {
+      let parameters = {};
+      if (typeof args[0] !== "number" || args[0] <= 0) {
+        throw new Error("Invalid base price input");
+      }
+      parameters.base = args[0];
+      if (args.length < 3) {
+        if (typeof args[1] === "string") {
+          parameters.category = args[1].toLowerCase();
+        }
+        parameters.ppl = 1;
+      } else {
+        parameters.ppl = args[1];
+        parameters.category = args[2].toLowerCase();
+      }
+      return parameters;
+    },
+
+    //match categories in CategoriesConfig(predefined object in config.js)
+    checkCategories: function(category, categories) {
+      for (let i in categories) {
+        if (categories[i].includes(category)) {
+          return i;
+        }
+      }
+      return undefined;
+    }
+  };
+};
+
+module.exports = Helper;
